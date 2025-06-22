70060cdc19102210daf4ba94f19cec31bc0e626a
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-06-22 18:56:13 +0530

    ...

---
+++ test_sample.py
@@ -0,0 +1,13 @@
+# test_sample.py
+
+def test_example_addition():
+    assert 1 + 1 == 2
+
+def test_example_subtraction():
+    assert 5 - 3 == 2
+
+def test_example_multiplication():
+    assert 3 * 4 == 12
+
+def test_example_division():
+    assert 8 / 2 == 4
